53665b39c7f0746cc97b2217acb2ab29e039e706
andres65 <andresbuenolopez65@gmail.com>
2023-09-14 17:17:12 -0500

    se agrega el proyecto back

---
+++ src/main/java/com/asmet/pruebaAsmetJava/PruebaAsmetJavaApplication.java
@@ -0,0 +1,13 @@
+package com.asmet.pruebaAsmetJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAsmetJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAsmetJavaApplication.class, args);
+	}
+
+}
